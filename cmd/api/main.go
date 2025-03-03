bf4ea97fb88edbee9d273cbdc09f2dff386ca236
Yakov <yakovx95@mail.ru>
2025-03-03 14:31:16 +0300

    light start

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
+)
 
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Practice project with codename - wateva")
+	})
+
+	log.Fatal(app.Listen(":3000"))
 }
